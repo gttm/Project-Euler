c062a3258b841a0f08f0962fa434e107c82f00db
George Touloupas <gttm_06@hotmail.com>
2016-06-09 23:48:54 +0300

    Add problem 12 solution

---
+++ euler012.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import math
+
+triangle = 0
+i = 1
+while 1:
+    triangle += i
+    i += 1
+    divisors = 1
+    j = 2
+    while j*j <= triangle:
+        if triangle%j == 0:
+            divisors += 1
+        j += 1
+#divisors under the root of the number are equal to the divisors over the root
+    divisors *= 2
+    if divisors > 500:
+        print triangle
+        break
